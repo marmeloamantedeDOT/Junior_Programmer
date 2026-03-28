7afc272d2f03a03ac0a753804b4e3d532ca6d95b
marmeloamantedeDOT <a63578@alunos.ipb.pt>
2026-03-28 19:11:50 +0000

    Lab 3

---
+++ Assets/Lab 2 e 3/Lab 3/PlayerControllerLab3.cs
@@ -0,0 +1,42 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerControllerLab3 : MonoBehaviour
+{
+    private float speed = 10;
+    private Rigidbody playerRb;
+    private float zBound = 6;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        ConstrainPlayerPosition();
+
+    }
+     void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+    void ConstrainPlayerPosition()
+    {
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+    }
+}
