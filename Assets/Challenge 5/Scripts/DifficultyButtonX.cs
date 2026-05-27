893dc819d4fb8a7500ce7afd382078650b1731fa
marmeloamantedeDOT <a63578@alunos.ipb.pt>
2026-05-27 23:49:16 +0100

    Challenge 5

---
+++ Assets/Challenge 5/Scripts/DifficultyButtonX.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButtonX : MonoBehaviour
+{
+    private Button button;
+
+    public GameManagerX gameManagerX;
+    public int difficulty;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    void SetDifficulty()
+    {
+        Debug.Log(gameObject.name + " was clicked");
+
+        gameManagerX.StartGame(difficulty);
+    }
+}
